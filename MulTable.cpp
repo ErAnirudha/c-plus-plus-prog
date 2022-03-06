3a83eca50397a2cf14e526525f5b9a74f4d39c0e
ErAnirudha <93269088+ErAnirudha@users.noreply.github.com>
2022-03-07 01:11:58 +0530

    create multiplication table which you want
    create multiplication table which you want take input from user

---
+++ MulTable.cpp
@@ -0,0 +1,13 @@
+//create mul table 17,37,115
+#include<iostream>
+using namespace std;
+int main(){
+	int x,num;
+	cout<<"Enter no which table You want to create =";
+	cin>>num;
+	for(int x=1;x<=10;x++){
+	cout<<num<<"*"<<x<<"="<<num*x<<endl;
+     }
+	
+	return 0;
+}
