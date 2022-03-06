01c54f91cb6e434b266c922d26b18d527c25543b
ErAnirudha <93269088+ErAnirudha@users.noreply.github.com>
2022-03-07 01:04:21 +0530

    Identify  Greater number between two numbers
    Identify  Greater number between two numbers Entered from user

---
+++ IdentifyGreaterNumber.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a,b;
+	cout<<"Enter any two numbers to identify which is greater";
+	cout<<"\nEnter first number:=";
+	cin>>a;
+	cout<<"\nEnter second number:=";
+	cin>>b;
+	if(a>b){
+		cout<<"\nFirst number is greater number ="<<a; 
+	}
+	else{
+		cout<<"\nsecond number is greater number ="<<b;
+	}
+	return 0;
+}
+
